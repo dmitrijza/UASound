896c7ea26bf0e0b4135dca13a91eae3a9dd41a25
Dmytro <dima-zagorodniy@ukr.net>
2023-01-09 03:18:05 +0100

    Base interface DataService

---
+++ src/main/java/org/uasound/data/service/DataService.java
@@ -0,0 +1,48 @@
+package org.uasound.data.service;
+
+import org.uasound.data.entity.DerivedData;
+import org.uasound.data.entity.DerivedMeta;
+import org.uasound.data.entity.GroupCard;
+import org.uasound.data.entity.SharedAudio;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface DataService {
+    void init();
+
+    void saveGroupCard(GroupCard card);
+
+    void saveData(DerivedData derivedData);
+
+    void saveAudio(SharedAudio audio);
+
+    boolean containsAudio(long internalId);
+
+    List<Long> getGroupIdList();
+
+    GroupCard getGroupCard(long groupId);
+
+    GroupCard getGroupCard(String groupTag);
+
+    List<Long> getCacheableData();
+
+    SharedAudio getAudio(long internalId);
+
+    DerivedMeta getMeta(long internalId);
+
+    DerivedData getData(long groupId, long postId);
+
+    DerivedData getData(final long internalId);
+
+    void updateData(DerivedData data);
+
+    void updateMeta(DerivedMeta meta);
+
+    void saveMeta(DerivedMeta meta);
+
+    boolean containsData(long groupId, long postId);
+
+    Collection<DerivedData> search(String query);
+
+}
